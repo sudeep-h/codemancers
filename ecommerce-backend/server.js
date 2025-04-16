f37f2b4ed1abd7d2044d708dd518bacf760cf450
sudeep-h <sudeephipparge@gmail.com>
2025-04-16 18:53:01 +0530

    Connected mongoDB

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -1,9 +1,26 @@
 const express=require('express');
-const app=express();
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const path=require('path');
+
 require('dotenv').config();
+
+const app=express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/public',express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
     res.send("Hello!");
+})
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MOngoDB connected successfully")
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed",err);
 })
 
 const PORT=process.env.PORT;
